fc9f8d5fae1ac5e8d53d4793d3243df3d2443d46
EdanZiv99 <ziv.edan@post.runi.ac.il>
2024-02-04 17:20:00 +0200

    Second try

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,17 +3,16 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-	int n = Integer.parseInt(args[0]); 
-	for (int j = 1 ; j <= n ; j++) {
-		if (j % 2 == 0) {
-			System.out.print(" ");
-		}
-		for (int i = 1 ; i <= n ; i++) {
-			System.out.print("* ");
-			if (i == n) {
+		int n = Integer.parseInt(args[0]); 
+		for (int j = 1 ; j <= n ; j++) {
+			for (int i = 0; i < n; i++)
+				if (j % 2 == 0) {
+					System.out.print("* ");
+				} else { 
+					System.out.print(" *");
+				}
 				System.out.println();				
 			}
 		}	
 	}
-}
-}
+	
