dc9bdad358fab9507b1c28d5979d121ff90170a7
EdanZiv99 <ziv.edan@post.runi.ac.il>
2024-02-05 10:04:09 +0200

    fifth try

--- OneOfEach.java
+++ OneOfEach.java
@@ -5,18 +5,22 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		int n = Math.random(); 
-		boolean IsBoy = false;
-		boolean IsGirl = false;  
-		String ans = ""; 
-		int sum = 0; 
-		while ((IsBoy = false) && (IsGirl = false)) {
-		if (n >= 0.5) {
-			ans = ans + "b";
-			}  else  {
-				ans = ans + "g";
-			}
-			n = 
+	boolean girl = false;
+	boolean boy = false; 
+	int count = 0;
+	while (!(girl && boy)){
+		double random = Math.random();
+		if (random <= 0.5){
+			girl = true; 
+			System.out.print("g ");
+		} else {
+			boy = true;
+			System.out.print("b ");
+		} 
+		count++; 
 		}
-	}
+		System.out.println();
+		System.out.println("You made it... and you have " + count + " children.");
+	}	
 }
+
--- OneOfEachStats1.java
+++ OneOfEachStats1.java
@@ -6,6 +6,56 @@
  */
 public class OneOfEachStats1 {
 	public static void main (String[] args) {
-		//// Put your code here
+		int T = Integer.parseInt(args[0]); 
+		double sumNumberOfChildren = 0;
+		int NumberOf2Children = 0;
+		int NumberOf3Children = 0;
+		int NumberOf4OrMoreChildren = 0;
+		
+		for (int i = 0 ; i < T ; i++) {
+			boolean girl = false;
+			boolean boy = false; 
+			int count = 0;  
+			while (!(girl && boy)){
+				double random = Math.random();
+				if (random <= 0.5){
+				girl = true; 
+				System.out.print("g ");
+				} else {
+					boy = true;
+					System.out.print("b ");
+				}
+				count++; 
+			}
+			System.out.println();
+			sumNumberOfChildren = sumNumberOfChildren + count;
+
+			if (count == 2) { 
+				NumberOf2Children++;
+			 }
+			if (count == 3) { 
+				NumberOf3Children++;
+			}
+			if (count >= 4) { 
+				NumberOf4OrMoreChildren++;
+			} 
+		}
+			String mostCommonNumber = ""; 
+			if ((NumberOf2Children > NumberOf3Children) && (NumberOf2Children > NumberOf4OrMoreChildren)) {
+				mostCommonNumber = "2."; 
+				if ((NumberOf3Children > NumberOf2Children) && (NumberOf3Children > NumberOf4OrMoreChildren)) {
+					mostCommonNumber = "3."; 
+				}
+					} else {
+				mostCommonNumber = "4 or more.";
+					
+			}
+		
+
+		System.out.println("Average: " + ((double) sumNumberOfChildren / T) + " children to get at least one of each gender.");
+		System.out.println("Number of families with 2 children: " + NumberOf2Children);
+		System.out.println("Number of families with 3 children: " + NumberOf3Children);
+		System.out.println("Number of families with 4 or more children: " + NumberOf4OrMoreChildren);
+		System.out.println("The most common number of children is " + mostCommonNumber) ;
 	}
-}
+}	
